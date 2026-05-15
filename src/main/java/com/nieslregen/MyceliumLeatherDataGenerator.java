c8c68abd0ef9e3cf3680518a3f1cec7200103468
Niels Asselborn <niels.asselborn@stud.uni-due.de>
2026-05-15 17:36:18 +0200

    Initial commit

---
+++ src/main/java/com/nieslregen/MyceliumLeatherDataGenerator.java
@@ -0,0 +1,11 @@
+package com.nieslregen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyceliumLeatherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
