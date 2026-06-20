2249a47e89c1c32f3282e334265ef40145901f34
Niels Asselborn <niels.asselborn@stud.uni-due.de>
2026-06-20 23:11:38 +0200

    Adjust Illness effect

--- src/main/java/com/nieslregen/effect/IllnessEffect.java
+++ src/main/java/com/nieslregen/effect/IllnessEffect.java
@@ -18,32 +18,31 @@ import java.util.Random;
 
 public class IllnessEffect extends MobEffect {
     private final RandomSource random;
+    private Vec3 leanDir = Vec3.ZERO;
 
     protected IllnessEffect(MobEffectCategory category, int color) {
-       random = RandomSource.create();
+        random = RandomSource.create();
+
         super(category, color);
     }
 
     @Override
     public boolean applyEffectTick(ServerLevel serverLevel, LivingEntity mob, int amplification) {
-        if (random.nextFloat() < 0.005F) {
-            Vec3 randomV = new Vec3(
-                    random.nextGaussian(),
-                    0,
-                    random.nextGaussian()
-            ).normalize().scale(0.1);
-
-            mob.addDeltaMovement(randomV);
-            mob.hurtMarked = true;
-            return true;
-        }
-
-        return applyEffectTick(serverLevel, mob, amplification);
+        mob.moveRelative(1, leanDir);
+        mob.hurtMarked = true;
+        return true;
     }
 
     @Override
     public void onEffectStarted(LivingEntity mob, int amplifier) {
         mob.addEffect(new MobEffectInstance(MobEffects.NAUSEA, 200, amplifier, false, false));
+
+        leanDir = new Vec3(
+                random.nextGaussian(),
+                0,
+                random.nextGaussian()
+        ).scale(0.025);
+
         super.onEffectStarted(mob, amplifier);
     }
 
